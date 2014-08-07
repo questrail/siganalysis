5418a2e430219afeb2d59431fd03c74d8e1de7a5
Matthew Rankin <matthew@questrail.com>
2014-08-07 11:13:20 -0500

    Closes #15

---
+++ CHANGES.md
@@ -0,0 +1,33 @@
+# Changelog
+
+### develop (unreleased)
+
+### v0.2
+
+#### Enhancements
+
+- Convert from Git Flow to Github Flow [#15][]
+- Automate PyPi deployment [#16][]
+- Add Travis-CI testing [#17][]
+
+### v0.1
+
+- Moved stft and smooth functions into siganalysis module.
+- Created `time_slice_zip()` function to create a zipped
+  list of tuples for time slicing a time series
+
+#### Bugs
+
+- Change `plot_spectrogram()` args [#2][]
+
+#### Enhancements
+
+- Make Google Python Style Guide compliant [#1][]
+- Add time range to `plot_spectrogram()` [#3][]
+
+[#1]: https://github.com/questrail/siganalysis/issues/1
+[#2]: https://github.com/questrail/siganalysis/issues/2
+[#3]: https://github.com/questrail/siganalysis/issues/3
+[#15]: https://github.com/questrail/siganalysis/issues/15
+[#16]: https://github.com/questrail/siganalysis/issues/16
+[#17]: https://github.com/questrail/siganalysis/issues/17
--- setup.py
+++ setup.py
@@ -1,7 +1,46 @@
-from distutils.core import setup
+import codecs
+import os
+import re
+
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    """Read parts of a file
+
+    Taken from pip's setup.py
+    intentionally *not* adding an encoding option to open
+    see: https://github.com/pypa/virtualenv/issues/201#issuecomment-3145690
+    """
+    return codecs.open(os.path.join(here, *parts), 'r').read()
+
+
+def find_version(*file_paths):
+    """Find version in source file
+
+    Read the version number from a source file.
+    Code taken from pip's setup.py
+    """
+    version_file = read(*file_paths)
+    # The version line must have the form:
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
 setup(
     name='siganalysis',
-    version='0.1',
+    version=find_version('siganalysis.py'),
     author='Matthew Rankin',
     author_email='matthew@questrail.com',
     py_modules=['siganalysis'],
