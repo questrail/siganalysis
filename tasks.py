5f6e53cda14034680d630472a69b1d068f7e2908
Matthew Rankin <matthew@questrail.com>
2022-07-05 07:25:00 -0500

    Update dependencies.

--- tasks.py
+++ tasks.py
@@ -5,40 +5,31 @@
 # can be found in the LICENSE.txt file for the project.
 """Invoke based tasks for siganalysis
 """
-from __future__ import print_function
-from __future__ import unicode_literals
-from __future__ import division
-from __future__ import absolute_import
-
 from invoke import run, task
 from unipath import Path
 
 ROOT_DIR = Path(__file__).ancestor(1)
-
 TESTPYPI = "https://testpypi.python.org/pypi"
 
 
 @task
 def lint(ctx):
     """Run flake8 to lint code"""
-    run("flake8")
+    run("python3 -m flake8")
+    run("python3 -m mypy siganalysis.py")
 
 
 @task
 def freeze(ctx):
-    """Freeze the pip requirements"""
-    run("pip freeze -l > {req}".format(
-        req=Path(ROOT_DIR, 'requirements.txt')))
+    # pylint: disable=W0613
+    """Freeze the pip requirements using pip-chill"""
+    run(f"pip-chill > {Path(ROOT_DIR, 'requirements.txt')}")
 
 
 @task(lint)
 def test(ctx):
-    """Lint and run unit tests"""
-    cmd = "{} {} {}".format(
-        "nosetests",
-        "--with-coverage --cover-erase",
-        "--cover-package=siganalysis --cover-html")
-    run(cmd)
+    """Lint, unit test, and check setup.py"""
+    run("nose2")
 
 
 @task()
@@ -56,7 +47,8 @@ def release(ctx, deploy=False, test=False, version=''):
             run("git tag -a v{ver} -m 'v{ver}'".format(ver=version))
             run("git push")
             run("git push origin --tags")
-            run("python setup.py register sdist upload")
+            run("python3 -m build")
+            run("python3 -m twine upload dist/*")
     else:
         print("* Have you updated the version?")
         print("* Have you updated CHANGELOG.md?")
